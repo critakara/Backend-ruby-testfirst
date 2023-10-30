d12048253c25366231894bdf7134c30c059bc2a1
Jane <116977119+critakara@users.noreply.github.com>
2023-10-30 17:50:17 -0400

    finished up

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,33 @@
+# ok so i need a list of 'small words' that won't be capitalized
+# i think this can just be local to the book class as it is
+
 class Book
-# write your code here
+  # write your code here
+  attr_accessor :title
+  
+  # when doing title case, always do the first word then do the checks
+  def title=(title)
+    littleWords = ["to", "and", "the", "in", "a", "of", "an"]
+    # ok so i need to split the title, do the first one, check through the small words
+    @title = title
+
+    titled = ""
+
+    words = @title.split()
+    titled = words[0].capitalize
+  
+    words.drop(1).each do |i|
+      if littleWords.include?(i)
+    # combine it
+        titled = titled + " " + i
+      else
+        titled = titled + " " + i.capitalize
+      end
+    end
+
+    @title = titled
+  end
 end
+
+
+
